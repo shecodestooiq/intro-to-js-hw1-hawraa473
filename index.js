e11216371b4252dbfb180733faa52ac562c28f7d
hawraa473 <hawraamuayad473@gmail.com>
2024-02-04 03:10:34 +0300

    task2

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var Age = 21
 var isstudent = true 
 console.log(" I am " , mYName);
 console.log(" I am " , Age);
-console.log("  I am a student " , isstudent); 
+console.log("  I am a student " , isstudent);   
+
+var Tocheck = false 
+console.log( " this var is " , typeof Tocheck); 
+
